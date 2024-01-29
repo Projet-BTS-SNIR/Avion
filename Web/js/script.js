d058b2b135bc00c42c4de89724a900892a91448c
Lil-_French_-21 <76883873+LilFrench21@users.noreply.github.com>
2024-01-29 08:36:48 +0100

    Add files via upload

--- Web/js/script.js
+++ Web/js/script.js
@@ -59,7 +59,7 @@ document.addEventListener('mouseup', () => {
     levierondown.hidden = true;
   }
 });
-
+   
 document.addEventListener('keydown', function(e) {
   if(e.code == "KeyZ")
   {
@@ -76,7 +76,7 @@ document.addEventListener('mousemove', (e) => {
   mouseY = e.clientY;
   if (isDragging == true) 
   {
-    const angle = Math.atan2(e.clientY - window.innerHeight / 2.5, e.clientX - window.innerWidth / 2.5);
+    const angle = Math.atan2(e.clientY - window.innerHeight / 2, e.clientX - window.innerWidth / 2);
     const rotation = angle * (180 / Math.PI);
     if(keypressZ == true)
     {
@@ -107,22 +107,147 @@ document.addEventListener('mousemove', (e) => {
   }
   if (isDraggingg == true) 
   {
-    if(rectangle.style.top < 268 + "px")
+    if(rectangle.style.top < 352 + "px")
     {
-      if(rectangle.style.top != 246 + "px")
+      if(rectangle.style.top != 337 + "px")
       {
         rectangle.style.top = (mouseY - 10) + 'px';
       }
     }
-    if(rectangle.style.top > 267 + 'px')
+    if(rectangle.style.top > 351 + 'px')
     {
-      rectangle.style.top = 267 + 'px';
+      rectangle.style.top = 351 + 'px';
       enlev = false;
     }
-    if(rectangle.style.top < 247 + 'px')
+    if(rectangle.style.top < 338 + 'px')
     {
-      rectangle.style.top = 245 + 'px';
+      rectangle.style.top = 336 + 'px';
       enlev = true;
     }
+  }
+});
+
+const strobelightgauche = document.getElementById("strobelightgauche");
+const ONstrobelightgauche = document.getElementById("ONstrobelightgauche");
+const OFFstrobelightgauche = document.getElementById("OFFstrobelightgauche");
+
+let strobelightgauchestatus = false;
+
+strobelightgauche.addEventListener("click", (event) => {
+  if(strobelightgauchestatus == true)
+  {
+    ONstrobelightgauche.style.color = "#808080";
+    ONstrobelightgauche.style.borderColor = "#808080";
+    OFFstrobelightgauche.style.color = "#FFFFFF";
+    OFFstrobelightgauche.style.borderColor = "#FFFFFF";
+    strobelightgauchestatus = false;
+  }
+  else
+  {
+    OFFstrobelightgauche.style.color = "#808080";
+    OFFstrobelightgauche.style.borderColor = "#808080";
+    ONstrobelightgauche.style.color = "#FF6800";
+    ONstrobelightgauche.style.borderColor = "#FF6800";
+    strobelightgauchestatus = true;
+  }
+});
+
+const strobelightdroite = document.getElementById("strobelightdroite");
+const ONstrobelightdroite = document.getElementById("ONstrobelightdroite");
+const OFFstrobelightdroite = document.getElementById("OFFstrobelightdroite");
+
+let strobelightdroitestatus = false;
+
+strobelightdroite.addEventListener("click", (event) => {
+  if(strobelightdroitestatus == true)
+  {
+    ONstrobelightdroite.style.color = "#808080";
+    ONstrobelightdroite.style.borderColor = "#808080";
+    OFFstrobelightdroite.style.color = "#FFFFFF";
+    OFFstrobelightdroite.style.borderColor = "#FFFFFF";
+    strobelightdroitestatus = false;
+  }
+  else
+  {
+    OFFstrobelightdroite.style.color = "#808080";
+    OFFstrobelightdroite.style.borderColor = "#808080";
+    ONstrobelightdroite.style.color = "#FF6800";
+    ONstrobelightdroite.style.borderColor = "#FF6800";
+    strobelightdroitestatus = true;
+  }
+});
+
+const aftstrobelight = document.getElementById("aftstrobelight");
+const ONaftstrobelight = document.getElementById("ONaftstrobelight");
+const OFFaftstrobelight = document.getElementById("OFFaftstrobelight");
+
+let aftstrobelightstatus = false;
+
+aftstrobelight.addEventListener("click", (event) => {
+  if(aftstrobelightstatus == true)
+  {
+    ONaftstrobelight.style.color = "#808080";
+    ONaftstrobelight.style.borderColor = "#808080";
+    OFFaftstrobelight.style.color = "#FFFFFF";
+    OFFaftstrobelight.style.borderColor = "#FFFFFF";
+    aftstrobelightstatus = false;
+  }
+  else
+  {
+    OFFaftstrobelight.style.color = "#808080";
+    OFFaftstrobelight.style.borderColor = "#808080";
+    ONaftstrobelight.style.color = "#FF6800";
+    ONaftstrobelight.style.borderColor = "#FF6800";
+    aftstrobelightstatus = true;
+  }
+});
+
+const taxilight = document.getElementById("taxilight");
+const ONtaxilight = document.getElementById("ONtaxilight");
+const OFFtaxilight = document.getElementById("OFFtaxilight");
+
+let taxilightstatus = false;
+
+taxilight.addEventListener("click", (event) => {
+  if(taxilightstatus == true)
+  {
+    ONtaxilight.style.color = "#808080";
+    ONtaxilight.style.borderColor = "#808080";
+    OFFtaxilight.style.color = "#FFFFFF";
+    OFFtaxilight.style.borderColor = "#FFFFFF";
+    taxilightstatus = false;
+  }
+  else
+  {
+    OFFtaxilight.style.color = "#808080";
+    OFFtaxilight.style.borderColor = "#808080";
+    ONtaxilight.style.color = "#FF6800";
+    ONtaxilight.style.borderColor = "#FF6800";
+    taxilightstatus = true;
+  }
+});
+
+const doubleflashlight = document.getElementById("doubleflashlight");
+const ONdoubleflashlight = document.getElementById("ONdoubleflashlight");
+const OFFdoubleflashlight = document.getElementById("OFFdoubleflashlight");
+
+let doubleflashlightstatus = false;
+
+doubleflashlight.addEventListener("click", (event) => {
+  if(doubleflashlightstatus == true)
+  {
+    ONdoubleflashlight.style.color = "#808080";
+    ONdoubleflashlight.style.borderColor = "#808080";
+    OFFdoubleflashlight.style.color = "#FFFFFF";
+    OFFdoubleflashlight.style.borderColor = "#FFFFFF";
+    doubleflashlightstatus = false;
+  }
+  else
+  {
+    OFFdoubleflashlight.style.color = "#808080";
+    OFFdoubleflashlight.style.borderColor = "#808080";
+    ONdoubleflashlight.style.color = "#FF6800";
+    ONdoubleflashlight.style.borderColor = "#FF6800";
+    doubleflashlightstatus = true;
   }
 });
